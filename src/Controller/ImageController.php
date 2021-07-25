3146c7b00d80f50ddcb035f45efcee634ee2eb08
JeffNys <sxb.jeanfrancois.nys@gmail.com>
2021-07-25 20:08:42 +0200

    fonction edit et delete

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -77,12 +77,34 @@ class ImageController extends AbstractController
     /**
      * @Route("/{id}/edit", name="image_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, Image $image): Response
+    public function edit(Request $request, Image $image, SluggerInterface $slugger): Response
     {
+        // il faut faire la même chose au niveau de l'édition
         $form = $this->createForm(ImageType::class, $image);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // si il y a une image il faut la placer la ou il faut
+            $imagesDirectory = "images/uploads/";
+            // donc, on commence par récuperer ce qui a été uploadé
+            $imageFile = $form->get('path')->getData();
+            // on test, au cas ou
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // on crée un nom unique de stockage du fichier
+                $safeFileName = $slugger->slug($originalFilename);
+                $finalFilename = $safeFileName . '-' . uniqid() . '.' . $imageFile->guessExtension();
+                // on essaye de deplacer le fichier à sa place finale, sur le serveur
+                $imageFile->move($imagesDirectory, $finalFilename);
+                // bon, il ne faudrait pas oublier de supprimer l'ancien fichier...
+                // donc, on commence par récupérer l'ancien nom
+                $oldFile = $image->getPath();
+                // ensuite, on peut mettre à jour le path dans l'objet image
+                $image->setPath($finalFilename);
+                // et on peut donc supprimmer l'ancien fichier
+                unlink("$imagesDirectory$oldFile");
+            }
+            // ensuite, il n'y a plus qu'à écrire tout ça dans la BDD
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('image_index', [], Response::HTTP_SEE_OTHER);
@@ -101,6 +123,11 @@ class ImageController extends AbstractController
     {
         if ($this->isCsrfTokenValid('delete'.$image->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
+            // on récupère l'ancien fichier et on le supprime
+            $imagesDirectory = "images/uploads/";
+            $oldFile = $image->getPath();
+            unlink("$imagesDirectory$oldFile");
+            // ensuite, il n'y a plus qu'a supprimer la ligne dans la BDD
             $entityManager->remove($image);
             $entityManager->flush();
         }
